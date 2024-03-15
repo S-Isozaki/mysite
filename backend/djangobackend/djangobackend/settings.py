c767d0f4a2b80d115ec5c898284707ad42101ed0
Isozaki Satoshi <i.satoshi1987@gmail.com>
2024-03-16 00:49:40 +0900

    fix environment

--- backend/djangobackend/djangobackend/settings.py
+++ backend/djangobackend/djangobackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangobackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restapidb',
+        'HOST': 'database',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'PORT': '3306',
     }
 }
 
